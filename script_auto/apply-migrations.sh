9df9a8e2e05152f3d83ea7f0622f3981f331b8ad
Sabbycor <sabrinacorrado5@gmail.com>
2026-01-30 21:49:58 +0100

    fix: script apply-mig modified

---
+++ script_auto/apply-migrations.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Attivazione del venv automatizzato"
+
+source venv/bin/activate
+
+echo "Entro nella cartella su cui stiamo lavorando"
+
+cd todolist_key
+
+python manage.py makemigrations
+
+echo "Migrazioni create, inizio a migrare"
+
+python manage.py migrate
+
+python manage.py runserver
+
+echo "Migrazioni applicate con successo!!"
